8b62e903d3d4c8c19c08ab82966f2c914bdd6dee
Dhawal M <dhawalmehta91@gmail.com>
2018-09-05 16:12:44 +0530

    init commit

---
+++ js/generic.js
@@ -0,0 +1,65 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Show the navbar when the page is scrolled up
+  var MQL = 992;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('#mainNav').height();
+    $(window).on('scroll', {
+      previousTop: 0
+    },
+      function () {
+        var currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+            $('#mainNav').addClass('is-visible');
+            var $source = $('.navbar-brand img');
+            $source.attr({
+              src: $source.attr('data-inverted')
+            })
+
+          } else {
+            $('#mainNav').removeClass('is-visible is-fixed');
+            var $source = $('.navbar-brand img');
+            $source.attr({
+              src: $source.attr('data-src')
+            })
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#mainNav').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+
+})(jQuery); // End of use strict
---
+++ js/init.js
@@ -0,0 +1,11 @@
+QueryLoader.selectorPreload = "#main_body";
+QueryLoader.init();
+
+$(document).ready(function() {
+
+  $("#main_event").hide();
+  // $("#start_event").show();
+  // $("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering();
+  $("#main_event").fadeIn(3000);
+  
+})
